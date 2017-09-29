b75a2ab45b71c4b76ea373dcb68b94dc00ad33f1
michal-jankowski <michaljankowski86@gmail.com>
2017-09-29 07:49:06 +0200

    updated

---
+++ js/backToTop-button.js
@@ -0,0 +1,42 @@
+(function () {
+
+    function createButton() {
+
+        var button = document.createElement("button");
+
+        button.classList.add("backToTop", "hidden");
+        button.textContent = "Powrót do góry";
+        document.body.appendChild(button);
+        
+        return button;
+        
+    }
+    
+    var button = createButton();
+
+    function animateScroll() {
+
+        if(document.body.scrollTop > 0 || window.pageYOffset > 0) {
+            window.scrollBy(0, -50);
+            setTimeout(animateScroll, 10);
+        }
+        
+    }
+    
+    button.addEventListener("click", function(e) {
+
+        e.stopPropagation();
+
+        animateScroll();
+        
+    }, false);
+
+    window.addEventListener("scroll", function(e) {
+        if(document.body.scrollTop >= 100 || window.pageYOffset >= 100) {
+            button.classList.remove("hidden");
+        } else {
+            button.classList.add("hidden");
+        }
+    }, false);
+    
+})();
